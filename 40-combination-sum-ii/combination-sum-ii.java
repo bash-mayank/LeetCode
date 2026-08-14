82cdb483f50b989622921d3241a99742e098f5d6
bash-mayank <mayankawasthi.edu@gmail.com>
2026-08-14 21:50:16 +0530

    Time: 7 ms (36.74%) | Memory: 48.5 MB (8.96%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> current = new ArrayList<>();
+        int sum = 0;
+        backtrack(0, result, current, nums, sum, target);
+        return result;
+    }
+
+    private void backtrack(int start, List<List<Integer>>result, ArrayList<Integer>current, int[] nums, int sum , int target){
+
+        if(sum == target){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        if(sum > target){
+            return;
+        }
+
+        for( int i = start ; i<nums.length; i++){
+            if(i> start && nums[i] == nums[i-1]) continue;
+            current.add(nums[i]);
+            sum = sum + nums[i];
+            backtrack(i+1, result, current, nums, sum, target);
+            sum -= nums[i];
+            current.remove(current.size()-1);
+            
+        }
+    }
+}
